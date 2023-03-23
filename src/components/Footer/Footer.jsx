cfec09742ac43d99ab22ad1fb802876ab300d5e9
Pavel <pal5555@mail.ru>
2023-03-23 18:07:00 +0300

    categories list

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { Link, } from 'react-router-dom'
+
+
+
+import styles from '../../styles/Footer.module.css'
+import { ROUTES } from '../../utils/routes'
+import LOGO from "../../images/logo.svg";
+
+
+
+
+const Footer = ()=>{
+    return (
+    <section className={styles.footer}>
+         <div className={styles.logo}>
+            <Link to ={ROUTES.HOME}>
+            <img src={LOGO} alt="LOGO" />
+
+            </Link>
+
+        </div>
+         <div className={styles.right}>
+            Developed by Pasha Zybkov
+         </div>
+         <div className={styles.socials}>
+        <a href="https://instagram.com"
+        targer="_blank"
+        real="noreferrer"
+        >
+           <svg className='icon'>
+                    <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#instagram`}/>
+
+                </svg>   
+
+        </a>
+        <a href="https://facebook.com"
+        targer="_blank"
+        real="noreferrer"
+        >
+           <svg className='icon'>
+                    <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#facebook`}/>
+
+                </svg>   
+
+        </a>
+        <a href="https://youtube.com"
+        targer="_blank"
+        real="noreferrer"
+        >
+           <svg className='icon'>
+                    <use xlinkHref={`${process.env.PUBLIC_URL}/sprite.svg#youtube`}/>
+
+                </svg>   
+
+        </a>
+
+         </div>
+
+    </section>
+    )
+}
+export default Footer
